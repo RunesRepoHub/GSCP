9267b5ed4a845f8b6ff6d8d5c3d7cbb500f382f9
rune <christian.rune1998@gmail.com>
2023-12-14 10:56:01 +0100

    Hotfix

--- Build.sh
+++ Build.sh
@@ -8,7 +8,7 @@ pg_path="/root/GSCP/Docker/Database"
 
 apt_path="/root/GSCP/Docker/API"
 
-compose="/root/GSCP/Docker/Docker-compose.yaml"
+compose="/root/GSCP/Docker/Docker-compose.yml"
 
 version=$(cat "$ver_path" | awk '{ print substr( $0, 9 ) }')
 ip_address=$(hostname -I | awk '{print $1}')
