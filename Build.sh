8ccbc25902ac8df6e487e0165620dec2461e64cd
rune <christian.rune1998@gmail.com>
2023-12-14 10:45:07 +0100

    Update

--- Build.sh
+++ Build.sh
@@ -1,4 +1,20 @@
-source ~/GSCP/Core.sh
+Version="curl https://raw.githubusercontent.com/RunesRepoHub/GSCP/Dev/Docker/.env | sed 's/.\{8\}//'"
+
+ver_path="/root/GSCP/Docker/.env"
+
+web_path="/root/GSCP/Docker/Web"
+
+pg_path="/root/GSCP/Docker/Database"
+
+apt_path="/root/GSCP/Docker/API"
+
+compose="/root/GSCP/Docker/Docker-compose.yaml"
+
+version=$(cat "$ver_path" | awk '{ print substr( $0, 9 ) }')
+ip_address=$(hostname -I | awk '{print $1}')
+
+dockerdb=$(docker ps | grep -i cnc-mysql: | awk '{print $2}')
+dockerweb=$(docker ps | grep -i cnc-web: | awk '{print $2}')
 
 docker build -t gscp-web:$version "$web_path"
 docker build -t gscp-pg:$version "$pg_path"
